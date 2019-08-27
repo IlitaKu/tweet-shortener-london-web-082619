ceec354fcd378f00e6521ddd7e17f090d22d8c5c
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-27 09:34:54 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,55 @@
-# Write your code here.
+require "pry"
+def dictionary 
+   {
+    hello: "hi",
+    to: "2",
+    two: "2",
+    too: "2",
+    for: "4",
+    four: "4",
+    be: "b",
+    you: "u",
+    at: "@",
+    and: "&"
+  }
+end
+def dictionary 
+   {
+    hello: "hi",
+    to: "2",
+    two: "2",
+    too: "2",
+    for: "4",
+    four: "4",
+    be: "b",
+    you: "u",
+    at: "@",
+    and: "&"
+  }
+end
+
+
+def word_substituter(tweet)
+  result = []
+  dict = dictionary
+  words = tweet.split(" ")
+
+  words.collect do |word|
+    dict.collect do |key, value|
+      if word == key 
+         word.replace(value)
+        binding.pry
+      end 
+    end
+  end 
+  word.join(" ")
+end 
+
+
+
+
+
+
+
+
+
